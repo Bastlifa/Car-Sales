07b3aa407930463771e763625e1dbcd425e4b6c2
Eli Sacks <bastlifa@gmail.com>
2019-08-14 12:18:43 -0400

    er, mvp maybe, forgot a lot of commits

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import { removeFeature } from '../actions'
 
 const AddedFeature = props => {
+  console.log('b',props.feature)
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick={_ => props.removeFeature(props.feature)}>X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+const mapStateToProps = state =>
+{
+  console.log('a', state)
+  return {
+    
+  }
+}
+
+export default connect(mapStateToProps, {removeFeature})(AddedFeature)
